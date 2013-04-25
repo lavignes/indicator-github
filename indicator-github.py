738d2ebbc6732d80dccef087e74d92b28f2b2d3f
Scott LaVigne <pyrated@gmail.com>
2013-04-24 17:04:46 -0700

    Add working prototype

--- indicator-github.py
+++ indicator-github.py
@@ -1,66 +1,92 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from github import Github
+import os
+import exceptions
+import getpass
 import webbrowser
-import gtk
+import urllib
+import requests
+import json
 import appindicator
+import gtk
 
-def login():
-    dialog = gtk.MessageDialog(
-                parent=None,
-                flags=0,
-                type=gtk.MESSAGE_QUESTION,
-                buttons=gtk.BUTTONS_OK_CANCEL,
-                message_format="Please enter your gitub credentials.")
+def find_token(items):
+  for d in items:
+    if d['app']['name'] == 'indicator-github':
+      return d['token']
+  else:
+    raise KeyError
 
-    dialog.set_title("Github login")
+client_id = '17d74debb1b3ef6337e6'
+redirect = 'http://pyrated.github.io/indicator-github/'
+request_string = 'https://github.com/login/oauth/authorize?client_id=%s&redirect_uri=%s&scope=%s'
+scope = ''
 
-    box = dialog.get_content_area()
-    password = gtk.Entry()
-    password.set_visibility(False)
-    password.set_invisible_char("*")
-    box.pack_end(password, False, False, 0)
+app_folder = os.path.expanduser(os.path.join('~','.indicator-github',''))
+if not os.path.exists(app_folder): os.makedirs(app_folder)
 
-    username = gtk.Entry()
-    username.set_visibility(True)
-    box.pack_end(username, False, False, 0)
+# Check if user has authenticated app
+if not os.path.isfile(os.path.join(app_folder,'oauth')):
+  # Authorize indicator-github
+  webbrowser.open(request_string % (urllib.quote(client_id), urllib.quote(redirect), urllib.quote(scope)))
 
-    dialog.show_all()
-    dialog.run()
+  # Get oauth tokens
+  session = requests.Session()
+  session.auth = (raw_input('username:'), getpass.getpass())
+  request = session.get('https://api.github.com/authorizations')
+  auths = json.loads(request.content)
 
-    gh = Github(username.get_text(), password.get_text())
-    dialog.destroy()
-    return gh
+  try:
+    token = find_token(auths)
+  except:
+    # User didn't authorize app
+    print 'Can\'t find token'
 
-def on_repo_clicked(menu_item, repo):
-  webbrowser.open(repo.html_url)
+  token_file = open(os.path.join(app_folder,'oauth'), 'w+')
+  token_file.write(token)
+  token_file.close()
 
-def on_name_clicked(menu_item, user):
-  webbrowser.open(user.html_url)
+# Auth token already exists
+else:
+  token_file = open(os.path.join(app_folder,'oauth'), 'r')
+  token = token_file.read()
 
-def create_menu(gh, ind):
-    menu = gtk.Menu()
+# Log user into github
+gh = Github(login_or_token=token, client_id=client_id, user_agent=client_id)
 
-    menu_item = gtk.MenuItem(gh.get_user().name)
-    menu_item.connect("activate", on_name_clicked, gh.get_user())
-    menu.append(menu_item)
+indicator = appindicator.Indicator ('indicator-github',\
+  os.path.join(os.getcwd(),'gh.png'),\
+  appindicator.CATEGORY_COMMUNICATIONS)
 
-    menu.append(gtk.SeparatorMenuItem())
-    
-    for repo in gh.get_user().get_repos():
-        menu_item = gtk.MenuItem(repo.name)
-        menu_item.connect("activate", on_repo_clicked, repo)
-        menu.append(menu_item)
+indicator.set_status(appindicator.STATUS_ACTIVE)
 
-    menu.show_all()
-    ind.set_menu(menu)
+menu = gtk.Menu()
 
-if __name__ == "__main__":
-    indicator = appindicator.Indicator ("indicator-brightness", \
-        "/home/pyrated/src/indicator-github/gh.png",\
-        appindicator.CATEGORY_COMMUNICATIONS)
-    
-    indicator.set_status(appindicator.STATUS_ACTIVE)
-    gh = login()
-    create_menu(gh, indicator)
-    gtk.main()
+menu_item = gtk.MenuItem(gh.get_user().name)
+menu.append(menu_item)
+
+menu.append(gtk.SeparatorMenuItem())
+
+repo_item = gtk.MenuItem("Repositories")
+repo_menu = gtk.Menu()
+repo_item.set_submenu(repo_menu)
+
+for repo in gh.get_user().get_repos():
+  menu_item = gtk.MenuItem(repo.name)
+  repo_menu.append(menu_item)
+
+menu.append(repo_item)
+
+menu.append(gtk.SeparatorMenuItem())
+
+menu_item = gtk.MenuItem("About...")
+menu.append(menu_item)
+
+menu_item = gtk.MenuItem("Quit")
+menu_item.connect("activate", gtk.mainquit)
+menu.append(menu_item)
+
+menu.show_all()
+indicator.set_menu(menu)
+gtk.main()
