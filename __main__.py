5eca539cf2cffbf1d230dc254aba3abfd180a1bc
Scott LaVigne <pyrated@gmail.com>
2013-04-26 00:41:41 -0700

    Update to use the amazing GObject Introspection

--- __main__.py
+++ __main__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 from github import Github
-import gtk as Gtk
-import gobject
+#import gtk as Gtk
+from gi.repository import Gtk, GObject, Notify
+from gi.repository import AppIndicator3 as AppIndicator
 import os
-import pynotify
+#import pynotify
 import exceptions
 import webbrowser
 import urllib
-import appindicator
 import datetime
 
 client_id = '17d74debb1b3ef6337e6'
@@ -16,15 +16,33 @@ request_string = 'https://github.com/login/oauth/authorize?client_id=%s&scope=%s
 scope = 'repo:status,notifications'
 token = ''
 app_folder = os.path.expanduser(os.path.join('~','.indicator-github',''))
-feed_time = datetime.datetime.utcnow()# - datetime.timedelta(days=1)
+feed_time = datetime.datetime.utcnow() - datetime.timedelta(hours=5)
 icon = os.path.join(os.getcwd(),'gh.png')
+old_events = []
+
+events = {
+  'CommitCommentEvent': 'commented on',
+  'CreateEvent': 'created',
+  'FollowEvent': 'started following',
+  'ForkEvent': 'forked',
+  'PublicEvent': 'open sourced',
+  'WatchEvent': 'starred'
+  }
+
+def initialize(gh):
+  read_events(gh)
+  # Check for updates every 2 minutes
+  GObject.timeout_add_seconds(2*60, read_events, gh)
+  return False
 
 def about(menu_item):
   dialog = Gtk.AboutDialog()
   dialog.set_name('Indicator Github')
   dialog.set_version('alpha')
   dialog.set_comments('An indicator applet for GitHub')
-  dialog.set_authors(['Scott LaVigne', 'Oriah Ulrich'])
+  dialog.set_authors(['Scott LaVigne', 'Oriah Ulrich'])  
+  with open('LICENSE.txt','r') as f:
+    dialog.set_license(f.read())
   dialog.show_all()
   dialog.run()
   dialog.destroy()
@@ -51,17 +69,22 @@ def credential_prompt():
   return auth
 
 def read_events(gh):
-  # Get my own events
+  print('Fetching events from GitHub')
   global feed_time
   for event in gh.get_user(gh.get_user().login).get_received_events():
     if event.created_at > feed_time:
-      pynotify.Notification(event.actor.login, event.type, icon).show()
+      # A repository could be deleted or something
+      try:
+        message = "%s %s" % (events[event.type], event.repo.full_name)
+        Notify.Notification.new(event.actor.login, message, None).show()
+      except: pass
     else:
       # If I reached an event that isn't new, then I know
       # there are no new event past it
       break
 
   feed_time = datetime.datetime.utcnow()
+  return True
 
 if not os.path.exists(app_folder): os.makedirs(app_folder)
 
@@ -69,7 +92,6 @@ if not os.path.exists(app_folder): os.makedirs(app_folder)
 if not os.path.isfile(os.path.join(app_folder,'oauth')):
   # Authorize indicator-github
   webbrowser.open(request_string % (urllib.quote(client_id), urllib.quote(scope)))
-
   # Get oauth token
   gh = Github(*credential_prompt(), client_id=client_id, user_agent=client_id)
   for auth in gh.get_user().get_authorizations():
@@ -78,7 +100,7 @@ if not os.path.isfile(os.path.join(app_folder,'oauth')):
       break
   else:
     raise KeyError
-
+  # Save token to file
   token_file = open(os.path.join(app_folder,'oauth'), 'w+')
   token_file.write(token)
   token_file.close()
@@ -90,15 +112,15 @@ else:
 
 gh = Github(login_or_token=token, client_id=client_id, user_agent=client_id)
 
-indicator = appindicator.Indicator(
+indicator = AppIndicator.Indicator.new(
   'indicator-github',
   icon,
-  appindicator.CATEGORY_COMMUNICATIONS)
-
-indicator.set_status(appindicator.STATUS_ACTIVE)
+  AppIndicator.IndicatorCategory.COMMUNICATIONS
+  )
+indicator.set_label('GitHub','test')
+indicator.set_status(AppIndicator.IndicatorStatus.ACTIVE)
 
 menu = Gtk.Menu()
-
 menu_item = Gtk.MenuItem(gh.get_user().name)
 menu.append(menu_item)
 
@@ -107,11 +129,9 @@ menu.append(Gtk.SeparatorMenuItem())
 repo_item = Gtk.MenuItem("Your Repositories")
 repo_menu = Gtk.Menu()
 repo_item.set_submenu(repo_menu)
-
 for repo in gh.get_user().get_repos():
   menu_item = Gtk.MenuItem(repo.name)
   repo_menu.append(menu_item)
-
 menu.append(repo_item)
 
 menu.append(Gtk.SeparatorMenuItem())
@@ -119,19 +139,15 @@ menu.append(Gtk.SeparatorMenuItem())
 menu_item = Gtk.MenuItem("About...")
 menu_item.connect("activate", about)
 menu.append(menu_item)
-
 menu_item = Gtk.MenuItem("Quit")
-menu_item.connect("activate", Gtk.mainquit)
+menu_item.connect("activate", Gtk.main_quit)
 menu.append(menu_item)
-
 menu.show_all()
 indicator.set_menu(menu)
 
-pynotify.init('indicator-github')
-pynotify.Notification('GitHub', 'Logged in', icon).show()
+# Initialize App in 1 second
+GObject.timeout_add_seconds(1, initialize, gh)
 
-# Check for updates every 2 minutes
-read_events(gh)
-gobject.timeout_add_seconds(2*60, read_events, gh)
-
+Notify.init('indicator-github')
+Notify.Notification.new('GitHub', 'Logged in', icon).show()
 Gtk.main()
