3b8eab65f653833b4c259f73c58814751193feee
Scott LaVigne <pyrated@gmail.com>
2013-04-25 20:32:29 -0700

    Repo read and Notification status

--- __main__.py
+++ __main__.py
@@ -1,49 +1,47 @@
 #!/usr/bin/env python
 
 from github import Github
+import gtk as Gtk
+import gobject
 import os
 import pynotify
 import exceptions
 import webbrowser
 import urllib
 import appindicator
-import gtk
-import gobject
 import datetime
 
 client_id = '17d74debb1b3ef6337e6'
-redirect = 'http://pyrated.github.io/indicator-github/'
-request_string = 'https://github.com/login/oauth/authorize?client_id=%s&redirect_uri=%s&scope=%s'
-scope = ''
+request_string = 'https://github.com/login/oauth/authorize?client_id=%s&scope=%s'
+scope = 'repo:status,notifications'
 token = ''
 app_folder = os.path.expanduser(os.path.join('~','.indicator-github',''))
-feed_time = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+feed_time = datetime.datetime.utcnow()# - datetime.timedelta(days=1)
 icon = os.path.join(os.getcwd(),'gh.png')
 
 def about(menu_item):
-  dialog = gtk.AboutDialog()
+  dialog = Gtk.AboutDialog()
   dialog.set_name('Indicator Github')
   dialog.set_version('alpha')
   dialog.set_comments('An indicator applet for GitHub')
-  dialog.set_authors(['Scott LaVigne'])
+  dialog.set_authors(['Scott LaVigne', 'Oriah Ulrich'])
   dialog.show_all()
   dialog.run()
   dialog.destroy()
 
 def credential_prompt():
-  dialog = gtk.MessageDialog(
+  dialog = Gtk.MessageDialog(
     parent=None,
     flags=0,
-    type=gtk.MESSAGE_QUESTION,
-    buttons=gtk.BUTTONS_OK_CANCEL,
-    message_format="Please enter your GitHub credentials.")
+    type=Gtk.MESSAGE_QUESTION,
+    buttons=Gtk.BUTTONS_OK_CANCEL,
+    message_format='Please enter your GitHub credentials.')
 
-  dialog.set_title("GitHub Login")
+  dialog.set_title('GitHub Login')
   box = dialog.get_content_area()
-  password = gtk.Entry()
+  password = Gtk.Entry()
   password.set_visibility(False)
-  password.set_invisible_char("*")
-  username = gtk.Entry()
+  username = Gtk.Entry()
   box.add(username)
   box.add(password)
   dialog.show_all()
@@ -70,7 +68,7 @@ if not os.path.exists(app_folder): os.makedirs(app_folder)
 # Check if user has authenticated app
 if not os.path.isfile(os.path.join(app_folder,'oauth')):
   # Authorize indicator-github
-  webbrowser.open(request_string % (urllib.quote(client_id), urllib.quote(redirect), urllib.quote(scope)))
+  webbrowser.open(request_string % (urllib.quote(client_id), urllib.quote(scope)))
 
   # Get oauth token
   gh = Github(*credential_prompt(), client_id=client_id, user_agent=client_id)
@@ -99,31 +97,31 @@ indicator = appindicator.Indicator(
 
 indicator.set_status(appindicator.STATUS_ACTIVE)
 
-menu = gtk.Menu()
+menu = Gtk.Menu()
 
-menu_item = gtk.MenuItem(gh.get_user().name)
+menu_item = Gtk.MenuItem(gh.get_user().name)
 menu.append(menu_item)
 
-menu.append(gtk.SeparatorMenuItem())
+menu.append(Gtk.SeparatorMenuItem())
 
-repo_item = gtk.MenuItem("Your Repositories")
-repo_menu = gtk.Menu()
+repo_item = Gtk.MenuItem("Your Repositories")
+repo_menu = Gtk.Menu()
 repo_item.set_submenu(repo_menu)
 
 for repo in gh.get_user().get_repos():
-  menu_item = gtk.MenuItem(repo.name)
+  menu_item = Gtk.MenuItem(repo.name)
   repo_menu.append(menu_item)
 
 menu.append(repo_item)
 
-menu.append(gtk.SeparatorMenuItem())
+menu.append(Gtk.SeparatorMenuItem())
 
-menu_item = gtk.MenuItem("About...")
+menu_item = Gtk.MenuItem("About...")
 menu_item.connect("activate", about)
 menu.append(menu_item)
 
-menu_item = gtk.MenuItem("Quit")
-menu_item.connect("activate", gtk.mainquit)
+menu_item = Gtk.MenuItem("Quit")
+menu_item.connect("activate", Gtk.mainquit)
 menu.append(menu_item)
 
 menu.show_all()
@@ -133,6 +131,7 @@ pynotify.init('indicator-github')
 pynotify.Notification('GitHub', 'Logged in', icon).show()
 
 # Check for updates every 2 minutes
+read_events(gh)
 gobject.timeout_add_seconds(2*60, read_events, gh)
 
-gtk.main()
+Gtk.main()
